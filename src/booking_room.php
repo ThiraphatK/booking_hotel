8e7c104eda43eef35603fba82484683225e14bb1
ThiraphatK <thiraphat_khar@cmu.ac.th>
2023-10-08 21:18:55 +0700

    starter

---
+++ src/booking_room.php
@@ -0,0 +1,25 @@
+<!--  จองห้องพัก
+- บริษัททัวร์
+- ชื่อผู้เข้าพัก
+    - คำนำหน้า
+    - ชื่อ
+    - นามสกุล
+    - อีเมล
+    - เบอร์โทร
+- เลือกหมายเลขห้องพัก
+- วันที่เข้าพัก
+- วันที่ออกห้องพัก
+- ปุ่มยืนยันการจอง
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ src/script/db_connect.php
@@ -0,0 +1,5 @@
+<?php
+$con = mysqli_connect("id21366898_booking_hotel", "id21366898_thiraphatk", "Karinchai0857077@", "id21366898_booking_hotel") or die("เกิดข้อผิดพลาดเกิดขึ้น");
+mysqli_set_charset($con, "utf8");
+
+?>
